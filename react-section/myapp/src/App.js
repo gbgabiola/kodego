27ba68a34f472a7d9dbec6442384429b69619027
Genesis Gabiola <gbgabiola@gmail.com>
2021-08-10 15:09:41 +0800

    Modify App.js

--- react-section/myapp/src/App.js
+++ react-section/myapp/src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const firstName = 'Genesis';
+  const lastName = 'Gabiola';
+  const gender = 'Male';
+  const age = 28;
+
+  const numArray = [4, 9, 16, 25];
+
+  const people = [
+    {
+      firstName: 'John',
+      lastName: 'Doe',
+      gender: 'male',
+      age: 37,
+    },
+    {
+      firstName: 'Mary',
+      lastName: 'Smith',
+      gender: 'female',
+      age: 43,
+    },
+    {
+      firstName: 'Lesley',
+      lastName: 'Scott',
+      gender: 'female',
+      age: 29,
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>
+        Hi! My name is {firstName} {lastName}. I am {age} years old. My gender
+        is {gender}.
+      </h1>
+      <p>
+        {numArray.join(', ')} square root is{' '}
+        {numArray.map(Math.sqrt).join(', ')}
+      </p>
+      <p>
+        People:{' '}
+        {people.map(person => (
+          <>
+            {person.firstName} {person.lastName} {person.gender} {person.age}
+          </>
+        ))}
+      </p>
+    </main>
   );
 }
 
